b0de826be420be3654111eb3d47a3d18c891b30d
Maxime <maxime.mouret@gmail.com>
2017-03-29 11:46:05 +0200

    initialization

---
+++ src/layouts/Homepage/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import LatestPosts from "../../components/LatestPosts"
+import Page from "../Page"
+
+const Homepage = (props) => {
+  return (
+  	
+    <Page { ...props }>
+      <LatestPosts />
+    </Page>
+  )
+}
+
+export default Homepage
